3cec63e331e3021be1a4dd5b70b3530a5f44ec81
Jeremyb59 <88490357+Jeremyb59@users.noreply.github.com>
2023-08-04 01:31:29 +0200

    2 files modified

--- ressources/js/module/popin.js
+++ ressources/js/module/popin.js
@@ -1,11 +1,18 @@
 export class Popin {
   constructor() {
-    console.log('Popin ok...');
+    console.log('Popin init ok...');
+
+    document.querySelector('#popin-close').addEventListener('click', function() {
+      document.querySelector('#popin').classList.add('display-none');
+    });
+
     const listePopin = document.querySelectorAll('[popin]');
 
     for(const element of listePopin) {
       element.addEventListener('click', function() {
-        console.log('click on button....' + this.nextSibling.innerHTML);
+        document.querySelector('#popin-content').innerHTML = this.nextSibling.innerHTML;
+
+        document.querySelector('#popin').classList.remove('display-none');
       });
     }
   }
